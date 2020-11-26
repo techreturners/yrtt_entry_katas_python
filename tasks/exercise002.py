c9f341945ddd9bd3d054db51954021fb5f465c1b
DilshadGit <dilshad.abdulla@icloud.com>
2020-11-26 17:02:48 +0000

    Collecting hour, minute and second to millisecond than add them together at the end as intiger to show the results of each time the input has been changed.

--- tasks/exercise002.py
+++ tasks/exercise002.py
@@ -2,5 +2,20 @@
 # Your task is to make the 'past' function return the time converted to milliseconds.
 # More examples in the test cases below.
 
+'''
+assert past(0,1,1) == 61000
+assert past(1,1,1) == 3661000
+assert past(1,0,1) == 3601000
+assert past(1,0,0) == 3600000
+'''
+
 def past(h, m, s):
-    return
+    h_to_millisec = h * 60 * 60 * 1000
+    m_to_millisec = m * 60 * 1000
+    s_to_millisec = s * 1000
+
+    result = int(h_to_millisec + m_to_millisec + s_to_millisec)
+
+    return result
+
+print(past(h=int(input('Enter h: ')), m=int(input('Enter m: ')), s=int(input('Enter s: '))))
